669e887d6c0857592e646c6424f00f0d3eb4c858
jskyzero <jskyzero@outlook.com>
2019-02-26 13:17:48 +0800

    fix class name error

--- Assets/Scripts/Roll-a-Ball/System/CameraController.cs
+++ Assets/Scripts/Roll-a-Ball/System/CameraController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraContriller : MonoBehaviour {
+public class CameraController : MonoBehaviour {
   public GameObject player;
   private Vector3 delta;
 
