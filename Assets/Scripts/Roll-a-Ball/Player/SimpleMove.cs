e4e570d8b537ee2cb60bbf968c8025ef2102a6f9
jskzyero <jskyzero@outlook.com>
2019-04-03 16:31:54 +0800

    start player move part

--- Assets/Scripts/Roll-a-Ball/Player/SimpleMove.cs
+++ Assets/Scripts/Roll-a-Ball/Player/SimpleMove.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleMove : MonoBehaviour {
-  public float speed;
+  public float speed = 2.0f;
+  public float jumpForce = 200.0f;
   public GameObject system;
   [SerializeField]
   private Rigidbody rb;
 
-  void Start() {
+  private void Start() {
     rb = GetComponent<Rigidbody>();
   }
 
-  void FixedUpdate() {
+  private void FixedUpdate() {
     float moveHorizontal = Input.GetAxis("Horizontal");
     float moveVertical = Input.GetAxis("Vertical");
 
     Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    Move(movement);
 
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      Jump();
+    }
+  }
+
+  private void Jump() {
+    Vector3 up = transform.TransformDirection(Vector3.up);
+    rb.AddForce(up * jumpForce);
+  }
+
+  private void Move(Vector3 movement) {
     rb.AddForce(movement * speed);
   }
 
-  void OnTriggerEnter(Collider other) {
+  private void OnTriggerEnter(Collider other) {
     if (other.gameObject.CompareTag("PickUp")) {
       other.gameObject.SetActive(false);
     }
