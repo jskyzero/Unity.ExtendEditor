08f288369be94fd8e5bdb894a01f822b458af046
jskyzero <jskyzero@outlook.com>
2019-04-27 21:28:53 +0800

    add save file  part

--- Assets/Editor/_Edior/TextFileEditor.cs
+++ Assets/Editor/_Edior/TextFileEditor.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
-using System.IO;
 
 // A Text File Editor support json, xml file format.
 // 
@@ -41,13 +42,15 @@ public class TextFileCustomEditor : Editor {
   private string loadString => File.ReadAllText(filePath);
   private TextAsset loadTextFile => Resources.Load(filePath) as TextAsset;
 
+  private static Dictionary<string, string> UIStringDict =
+    new Dictionary<string, string> { { "SaveButtonName", "保存文件" },
+      { "ReloadButtonName", "重新加载" },
+      { "NeedStyleButtonName", "格式化文档" },
+    };
+
   // constructor
   TextFileCustomEditor() : base() {
-    Debug.Log("TextFileCustomEditor Construct");
-
-    // textString = ;
-    // TextAsset textFile = Resources.Load(filePath) as TextAsset;
-    // Debug.Log(textFile);
+    // Debug.Log("TextFileCustomEditor Construct");
   }
 
   // on gui
@@ -60,12 +63,33 @@ public class TextFileCustomEditor : Editor {
 
   private void OnGUI_Main() {
     if (textString.Equals(string.Empty)) textString = textAssetString;
-    textString = EditorGUILayout.TextArea(textString);
+
+    OnGUI_TitlePart();
+
+    GUIStyle style = new GUIStyle();
+    style.richText = true;
+    textString = EditorGUILayout.TextArea(textString, style);
   }
 
-  private void OnGUI_Header() {
+  private void OnGUI_TitlePart() {
+    GUILayout.BeginHorizontal(EditorStyles.toolbar);
 
+    if (GUILayout.Button(UIStringDict["SaveButtonName"], EditorStyles.toolbarButton)) { }
+    GUILayout.Space(5);
+    if (GUILayout.Button("重新加载", EditorStyles.toolbarButton)) { }
+    GUILayout.FlexibleSpace();
+    GUILayout.Toggle(true,
+      "Preview", EditorStyles.toolbarButton);
+
+    GUILayout.EndHorizontal();
   }
 
-  // private void 
+  private void SaveFile() {
+    File.WriteAllText(filePath, textString);
+  }
+
+  // Override this method to return false if you don't want default margins.
+  public override bool UseDefaultMargins() {
+    return false;
+  }
 }
