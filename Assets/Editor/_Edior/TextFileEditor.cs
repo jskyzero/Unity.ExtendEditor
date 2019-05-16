5810270aefdeb664b6770ca9c34d6e0abe815b97
jskyzero <jskyzero@outlook.com>
2019-05-16 18:38:33 +0800

    add json and xml part

--- Assets/Editor/_Edior/TextFileEditor.cs
+++ Assets/Editor/_Edior/TextFileEditor.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,29 +14,30 @@ using UnityEngine;
 // 2. with style
 // 3. compare with another
 
-public class TextFileEditor : EditorWindow {
-  string myString = "Hello World";
-  bool groupEnabled;
-  bool myBool = true;
-  float myFloat = 1.23f;
+// // seems no need to create a individual windows
+// public class TextFileEditor : EditorWindow {
+//   string myString = "Hello World";
+//   bool groupEnabled;
+//   bool myBool = true;
+//   float myFloat = 1.23f;
 
-  // Add menu item named "Text File Editor" to the Window menu
-  [MenuItem("Jsky/Text File Editor")]
-  public static void ShowWindow() {
-    //Show existing window instance. If one doesn't exist, make one.
-    EditorWindow.GetWindow(typeof(TextFileEditor));
-  }
+//   // Add menu item named "Text File Editor" to the Window menu
+//   [MenuItem("Jsky/Text File Editor")]
+//   public static void ShowWindow() {
+//     //Show existing window instance. If one doesn't exist, make one.
+//     EditorWindow.GetWindow(typeof(TextFileEditor));
+//   }
 
-  void OnGUI() {
-    GUILayout.Label("Hello", EditorStyles.boldLabel);
-    myString = EditorGUILayout.TextField("Text Field", myString);
+//   void OnGUI() {
+//     GUILayout.Label("Hello", EditorStyles.boldLabel);
+//     myString = EditorGUILayout.TextField("Text Field", myString);
 
-    groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-    myBool = EditorGUILayout.Toggle("Toggle", myBool);
-    myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-    EditorGUILayout.EndToggleGroup();
-  }
-}
+//     groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+//     myBool = EditorGUILayout.Toggle("Toggle", myBool);
+//     myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+//     EditorGUILayout.EndToggleGroup();
+//   }
+// }
 
 [CustomEditor(typeof(TextAsset), true)]
 public class TextFileCustomEditor : Editor {
@@ -43,10 +48,14 @@ public class TextFileCustomEditor : Editor {
   private string loadString => File.ReadAllText(filePath);
   private TextAsset loadTextFile => Resources.Load(filePath) as TextAsset;
 
+  private bool needStyle = false;
+  private bool saveWithoutStyle = true;
+
   private class UIStringDict {
     public string SaveButtonName = "保存文件";
     public string ReloadButtonName = "重新加载";
     public string NeedStyleButtonName = "格式化文档";
+    public string SaveWithoutStyle = "无格式保存";
   }
 
   private static UIStringDict kUIStringDict = new UIStringDict();
@@ -61,7 +70,7 @@ public class TextFileCustomEditor : Editor {
     // use a action
     Action<Action> enable = (action) => {
       bool enabled = GUI.enabled;
-      GUI.enabled = true; 
+      GUI.enabled = true;
       action.Invoke();
       GUI.enabled = enabled;
     };
@@ -70,28 +79,47 @@ public class TextFileCustomEditor : Editor {
   }
 
   private void OnGUI_Main() {
-    if (textString.Equals(string.Empty)) textString = textAssetString;
+    if (textString.Equals(string.Empty))
+      textString = textAssetString;
 
     OnGUI_TitlePart();
-
-    GUIStyle style = new GUIStyle();
-    style.richText = true;
-    textString = EditorGUILayout.TextArea(textString, style);
+    OnGUI_MainPart();
   }
 
   private void OnGUI_TitlePart() {
     GUILayout.BeginHorizontal(EditorStyles.toolbar);
 
-    if (GUILayout.Button(kUIStringDict.SaveButtonName, EditorStyles.toolbarButton)) {
+    if (GUILayout.Button(kUIStringDict.SaveButtonName,
+        EditorStyles.toolbarButton)) {
       SaveFile();
     }
     GUILayout.Space(5);
-    if (GUILayout.Button(kUIStringDict.ReloadButtonName, EditorStyles.toolbarButton)) { }
+    if (GUILayout.Button(kUIStringDict.ReloadButtonName,
+        EditorStyles.toolbarButton)) {
+      ReloadFile();
+    }
+    GUILayout.Space(5);
+    if (GUILayout.Button("TEST Format",
+        EditorStyles.toolbarButton)) {
+      FormatString("");
+    }
     GUILayout.FlexibleSpace();
-    GUILayout.Toggle(true,
-      "Preview", EditorStyles.toolbarButton);
+    needStyle = GUILayout.Toggle(needStyle,
+      kUIStringDict.NeedStyleButtonName, EditorStyles.toolbarButton);
+    if (needStyle) {
+      GUILayout.Space(5);
+      saveWithoutStyle = GUILayout.Toggle(saveWithoutStyle,
+        kUIStringDict.SaveWithoutStyle, EditorStyles.toolbarButton);
+    }
 
     GUILayout.EndHorizontal();
+  }
+
+  private void OnGUI_MainPart() {
+    // GUIStyle style = new GUIStyle();
+    // style.richText = true;
+    // textString = EditorGUILayout.TextArea(textString, style);
+    textString = EditorGUILayout.TextArea(textString);
   }
 
   private void ReloadFile() {
@@ -100,6 +128,36 @@ public class TextFileCustomEditor : Editor {
 
   private void SaveFile() {
     File.WriteAllText(filePath, textString);
+  }
+
+  private static string FormatString(string originString) {
+    originString = "";
+    string finalString = "";
+
+    Func<string, string> formatJsonString = json => {
+      dynamic parsedJson = JsonConvert.DeserializeObject(json);
+      return JsonConvert.SerializeObject(parsedJson, Newtonsoft.Json.Formatting.Indented);
+    };
+
+    Func<string, string> formatXmlString = xml => {
+      var stringBuilder = new StringBuilder();
+      var element = XElement.Parse(xml);
+      var settings = new XmlWriterSettings() {
+        OmitXmlDeclaration = true,
+        Indent = true,
+        NewLineOnAttributes = true,
+      };
+      using(var xmlWriter = XmlWriter.Create(stringBuilder, settings)) {
+        element.Save(xmlWriter);
+      }
+      return stringBuilder.ToString();
+    };
+
+    Func<string, string> formatOriginString = str => {
+      return str;
+    };
+
+    return finalString;
   }
 
   // Override this method to return false if you don't want default margins.
