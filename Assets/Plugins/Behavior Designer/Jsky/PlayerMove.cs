72a82846eb8807581a10d109eca33bded1a706e9
jskyzero <jskyzero@outlook.com>
2021-06-25 14:11:23 +0800

    ✨ add auto find ai

--- Assets/Plugins/Behavior Designer/Jsky/PlayerMove.cs
+++ Assets/Plugins/Behavior Designer/Jsky/PlayerMove.cs
@@ -2,26 +2,33 @@ using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using UnityEngine;
 
-namespace BehaviorDesigner.Runtime.Tasks.Jsky {
-
+namespace BehaviorDesigner.Runtime.Tasks.Jsky
+{
   [TaskDescription("Simple Move")]
   [TaskCategory("Jsky")]
-  public class PlayerMove : Action {
-    [Tooltip("target")]
-    public SharedGameObject target;
-    [Tooltip("speed")]
-    public float speed;
-    [Tooltip("break space")]
-    public float space = 0.05f;
+  public class PlayerMove : Action
+  {
+    [Tooltip("target")] public SharedGameObject target;
+    [Tooltip("speed")] public float speed;
+    [Tooltip("break space")] public float space = 0.05f;
 
-    public override TaskStatus OnUpdate() {
+    public override TaskStatus OnUpdate()
+    {
+      if (null == target.Value)
+      {
+        return TaskStatus.Failure;
+      }
+
       Vector3 ZeroY = new Vector3(1, 0, 1);
       Vector3 distence = Vector3.Scale(transform.position, ZeroY) -
-        Vector3.Scale(target.Value.transform.position, ZeroY);
-      if (Vector3.SqrMagnitude(distence) < space) {
+                         Vector3.Scale(target.Value.transform.position, ZeroY);
+      if (Vector3.SqrMagnitude(distence) < space)
+      {
         return TaskStatus.Success;
       }
-      var movePostion = Vector3.MoveTowards(transform.position, target.Value.transform.position, speed * Time.deltaTime);
+
+      var movePostion =
+        Vector3.MoveTowards(transform.position, target.Value.transform.position, speed * Time.deltaTime);
       transform.position = new Vector3(movePostion.x, transform.position.y, movePostion.z);
       return TaskStatus.Running;
     }
